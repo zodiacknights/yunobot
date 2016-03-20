4bd76367886324b6e4144b6de122017ad2883231
Nate Parrish <nate.parrish@arrowpointe.com>
2016-03-20 13:52:04 -0700

    Promisify exec

--- yuno/privateCommand.js
+++ yuno/privateCommand.js
@@ -1,7 +1,7 @@
 module.exports = function(localData, send){
 	'use strict';
-	var exec = require('child_process').exec;
 	var Promise = require('bluebird');
+	var exec = Promise.promisify(require('child_process').exec);
 	var fs = Promise.promisifyAll(require('fs'));
 
 	function getPath(path){
@@ -13,7 +13,7 @@ module.exports = function(localData, send){
 	}
 
 	function handleErr(err, room){
-		send(room, 'uh oh, it didn\'t work because: ' + err);
+		send(room, 'uh oh, it didn\'t work' + (err ? ' because:\n' + err : '.'));
 	}
 
 	return {
@@ -24,10 +24,11 @@ module.exports = function(localData, send){
 			});
 		},
 		cmd: function(name, room, arr){
-			var cmd = ('cd ' + getPath(arr[2]) ? getPath(arr[2]) : arr[2]) + ' | ' +  arr.slice(3).join(' ');
-			exec(cmd, function(err, stdout, stderr){
-				if(err) send(room, 'i\'m sorry, i couldn\'t make it work.' + (stderr ? 'it says:\n' + stderr : ''));
-				else send(room, '*yells command...* it worked! ' + (stdout ? ' it says:\n' + stdout : ''));
+			var cmd = 'cd ' + (getPath(arr[2]) ? getPath(arr[2]) : arr[2]) + ' | ' +  arr.slice(3).join(' ');
+			exec(cmd).then(function(result){
+				send(room, '*yells command...* it worked! ' + (result.stdout ? ' it says:\n' + result.stdout : ''));
+			}).catch(function(err){
+				handleErr(err, room);
 			});
 		},
 		commit: function(name, room, arr){
@@ -36,8 +37,10 @@ module.exports = function(localData, send){
 			commitMsg = commitMsg.slice(0, 1).toUpperCase() + commitMsg.slice(1);
 			var cmd = 'cd ' + getPath(arr[2]) + ' | git commit -am "' + commitMsg + '"';
 			send(room, '*writes commit mesage...* there, a hand written note.', function(){
-				exec(cmd, function(err, stdout, stderr){
-					if(err) handleErr(err, room);
+				exec(cmd).then(function(stdout){
+					console.log(stdout);
+				}).catch(function(err){
+					handleErr(err, room);
 				});
 			});
 		},
@@ -50,25 +53,28 @@ module.exports = function(localData, send){
 		pull: function(name, room, arr){
 			if(!getPath(arr[2])) return pathNotFound(room);
 			var cmd = 'cd ' + getPath(arr[2]) + ' | git pull upstream master';
-			exec(cmd, function(err){
-				if(err) handleErr(err, room);
-				else send(room, '*pulls with all her strength..* i did it, pull complete!');
+			exec(cmd).then(function(){
+				send(room, '*pulls with all her strength..* i did it, pull complete!');
+			}).catch(function(err){
+				handleErr(err, room);
 			});
 		},
 		push: function(name, room, arr){
 			if(!getPath(arr[2])) return pathNotFound(room);
 			var cmd = 'cd ' + getPath(arr[2]) + ' | git push origin ' + arr[3];
-			exec(cmd, function(err){
-				if(err) handleErr(err, room);
-				else send(room, '*hnnng...* there we go, ' + arr[2] + ' push complete!');
+			exec(cmd).then(function(){
+				send(room, '*hnnng...* there we go, ' + arr[2] + ' push complete!');
+			}).catch(function(err){
+				handleErr(err, room);
 			});
 		},
 		rebase: function(name, room, arr){
 			if(!getPath(arr[2])) return pathNotFound(room);
 			var cmd = 'cd ' + getPath(arr[2]) + ' | git pull --rebase upstream master';
-			exec(cmd, function(err){
-				if(err) handleErr(err, room);
-				else send(room, '*sorts commits...* let\'s see... this one goes here... aaand rebase complete!');
+			exec(cmd).then(function(){
+				send(room, '*sorts commits...* let\'s see... this one goes here... aaand rebase complete!');
+			}).catch(function(err){
+				handleErr(err, room);
 			});
 		},
 		seppuku: function(name, room, arr){
