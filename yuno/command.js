bed2ce25dea5c6a422a589b5da050b3c62da8ac9
ajora <ajora@rocketmail.com>
2016-03-26 14:33:52 -0700

    Weather forecast function added

--- yuno/command.js
+++ yuno/command.js
@@ -50,11 +50,54 @@ module.exports = function(localData, send){
 				url:"https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + location + "')&format=json"
 			}).then(function(res){
 				var data = JSON.parse(res.body);
-				var message = "It\'s currently " + data.query.results.channel.item.condition.temp + "\u2109 in " + location + "."
+				var status = '';
+				data = data.query.results.channel.item.condition;
+				if (data.code === '32' || data.code === '34'){
+					status = ' :sunny::relaxed::sunny:';
+				}
+				else if(data.code === '9' || data.code === '11'|| data.code === '12' || data.code === '40'){
+					status = ' :sweat_drops::confounded::umbrella:' + " Take an umbrella!";
+				}
+				else if(data.code === '16' || data.code === '17' || data.code === '41' || data.code === '43'){
+					status = ' :snowflake::snowman::snowflake: ';
+				}
+				else if(parseInt(data.code) > 25 && parseInt(data.code) < 31){
+					status= ' :cloud::star2::cloud:';
+				}
+				var message = 'It\'s ' + data.temp + '\u2109 in ' + location + '. ' + data.text + '!' + status;
 				send(room, message);
 				}).catch(function(err){
 				console.log(err);
 			});
+		},
+		forecast: function(name, room, arr){
+			var location = _.capitalize(arr[2]);
+			if (arr[3]) location += " " + _.capitalize(arr[3]);		
+			request.getAsync({
+				url:"https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + location + "')&format=json"
+			}).then(function(res){
+				var highs = 'The next three day highs are:';
+				var lows = 'The next three day lows are:';
+				var message = '';
+				var data = JSON.parse(res.body);
+
+				data = data.query.results.channel.item.forecast.slice(0, 3);
+				_.forEach(data, function(forecast, key){
+					console.log(key)
+					if (key < 2){
+					  highs += ' ' + forecast.high + '\u2109,';
+					  lows += ' ' + forecast.low + '\u2109,';
+				  }
+				  else{
+				  	highs += ' and ' + forecast.high + '\u2109!';
+				  	lows += ' and ' + forecast.low + '\u2109.';
+				  }
+				});
+				message += highs + " " +lows;
+				send(room, message);
+				}).catch(function(err){
+				console.log(err);
+			});	
 		}
 	};
 };
