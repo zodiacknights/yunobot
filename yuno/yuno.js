9e1c4a9a3243d5b53788bac5d6dbcd030e9c7899
Ajora <thelaughingman@ymail.com>
2018-01-05 15:13:50 -0800

    initial janus update

--- yuno/yuno.js
+++ yuno/yuno.js
@@ -1,65 +1,57 @@
 const localData = require('../LOCAL_DATA.json');
 const commands = require('../commands/index.js');
 const privateCommand = require('./privateCommand');
+const lobby = [];
 
-  function checkRoom(room){
-    return room === localData.privateroom;
+function checkRoom(room){
+  return room === localData.privateroom;
+}
+function listLobby(message) {
+  let lobbyMsg = [];
+  for (let i = 0; i < lobby.length; i++) {
+    lobbyMsg.push(`${i+1}) ${lobby[i]}`);
   }
+  message.channel.sendMessage(lobbyMsg);
+}
 
-  const defaultResponses = [
-    ['how do i ', '? :confused:'],
-    ['*tries her best to ', '.*'],
-    ['how about you ', '. :smirk:'],
-    ['*puts knife away* ok, i\'ll ', ' instead.'],
-  ];
+function getDefaultResponse(message){
+  var num = Math.floor(Math.random() * defaultResponses.length);
+  return defaultResponses[num][0] + message + defaultResponses[num][1];
+}
 
-  function getDefaultResponse(message){
-    var num = Math.floor(Math.random() * defaultResponses.length);
-    return defaultResponses[num][0] + message + defaultResponses[num][1];
+function parse(message){
+  message = message.replace(/[\*]/g, '');
+  message = message.toLowerCase();
+  return message.split(' ');
+}
+function sendQuery(name, arr){
+  return commands[arr[1]](name,arr);
+}
+function handleMessage(message){
+  if (message.author.bot) return;
+  console.log(message)
+  const name = message.author.username;
+  const arr = parse(message.content);
+  const privateCommandObj = privateCommand(localData,
+    message.channel.sendMessage.bind(message.channel));
+    console.log(arr)
+  if (arr[0].includes('steam://joinlobby/')) {
+    lobby.push(`${message.author.username}: ${message.content}`);
+    listLobby(message);
   }
+  if (arr[0] !== 'janus') return;
+  if (arr[1].includes('list')) {
+    listLobby(message);
+  }
+  if (arr[1].includes('close')) {
+    lobby.splice(parseInt(arr[2]) - 1, 1);
+    listLobby(message);
+  }
+};
 
-  function parse(message){
-    message = message.replace(/[\*]/g, '');
-    message = message.toLowerCase();
-    return message.split(' ');
-  }
-  function sendQuery(name, arr){
-    return commands[arr[1]](name,arr);
-  }
-  function handleMessage(message){
-    if (message.author.bot) return;
-    const name = message.author.username;
-    const arr = parse(message.content);
-    const privateCommandObj = privateCommand(localData,
-      message.channel.sendMessage.bind(message.channel));
-    if (checkRoom(message.channel.id)) {
-      console.log('(private)');
-      if (arr[0].slice(0, 4) !== 'yuno') arr.unshift('yuno');
-    }
-    if (arr[0] !== 'yuno') return;
-    if (checkRoom(message.channel.id) && privateCommandObj[arr[1]]) {
-      privateCommandObj[arr[1]](name, arr);
-    } else if (commands[arr[1]]) {
-      const result = sendQuery(name, arr);
-      if (typeof result === 'string') {
-        message.channel.sendMessage(result);
-      } else {
-        result.then(function (msg) {
-          message.channel.sendMessage(msg);
-        }).catch((err) => {
-          message.channel.sendMessage(`uh oh, it didn't work because:\n${err}`);
-        });
-      }
-    } else if (!arr[1]) {
-      message.channel.sendMessage('hmm?');
-    } else {
-      message.channel.sendMessage(getDefaultResponse(arr.slice(1).join(' ')));
-    }
-  };
-
-  module.exports = {
-    checkRoom,
-    getDefaultResponse,
-    parse,
-    handleMessage
-  }
+module.exports = {
+  checkRoom,
+  getDefaultResponse,
+  parse,
+  handleMessage
+}
